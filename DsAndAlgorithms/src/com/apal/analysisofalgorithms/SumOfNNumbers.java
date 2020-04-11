68fa030ebd6b401e422240882ff4fe0c12f4ed2a
anilkumarpal1608 <anilkumarpal.1608@gmail.com>
2020-04-11 20:51:11 +0530

    Initial Commit

---
+++ DsAndAlgorithms/src/com/apal/analysisofalgorithms/SumOfNNumbers.java
@@ -0,0 +1,21 @@
+package com.apal.analysisofalgorithms;
+
+public class SumOfNNumbers {
+
+	int getSumofNNumbers(int n) {
+		int sum = 0;
+		for(int i=1 ; i<=n ; i++ ) {
+			sum+=i;
+		}
+		return sum;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 10;
+		SumOfNNumbers s = new SumOfNNumbers();
+		
+		System.out.println("Sum:"+s.getSumofNNumbers(n));
+	}
+
+}
